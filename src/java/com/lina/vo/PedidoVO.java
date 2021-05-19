c5d12da2802ca6d8da2f867c3a7593f311f5dcd8
Nidia Herrera <neherrera0@misena.edu.co>
2021-05-19 14:32:41 -0500

    añade pedido VO

--- src/java/com/lina/vo/PedidoVO.java
+++ src/java/com/lina/vo/PedidoVO.java
@@ -5,6 +5,8 @@
  */
 package com.lina.vo;
 
+import java.sql.Date;
+
 /**
  *
  * @author Nidia
@@ -12,53 +14,35 @@ package com.lina.vo;
 public class PedidoVO {
 
     //Mensaje Jose Nidia github
-    
-    private String id_Pedido, id_Usuario, fecha_Pedido,
-            fecha_Entrega, forma_Envio;
-    
+    private int id_Pedido, id_Usuario;
+    private String forma_Envio;
+    private Date fecha_Pedido, fecha_Entrega;
+
     public PedidoVO() {
     }
-    
-    public PedidoVO(String id_Pedido, String id_Usuario, String fecha_Pedido, String fecha_Entrega, String forma_Envio) {
+
+    public PedidoVO(int id_Pedido, int id_Usuario, Date fecha_Pedido, Date fecha_Entrega, String forma_Envio) {
         this.id_Pedido = id_Pedido;
         this.id_Usuario = id_Usuario;
+        this.forma_Envio = forma_Envio;
         this.fecha_Pedido = fecha_Pedido;
         this.fecha_Entrega = fecha_Entrega;
-        this.forma_Envio = forma_Envio;
     }
 
-   
-
-    public String getId_Pedido() {
+    public int getId_Pedido() {
         return id_Pedido;
     }
 
-    public void setId_Pedido(String id_Pedido) {
+    public void setId_Pedido(int id_Pedido) {
         this.id_Pedido = id_Pedido;
     }
 
-    public String getId_Usuario() {
+    public int getId_Usuario() {
         return id_Usuario;
     }
 
-    public void setId_Usuario(String id_Usuario) {
+    public void setId_Usuario(int id_Usuario) {
         this.id_Usuario = id_Usuario;
-    }
-
-    public String getFecha_Pedido() {
-        return fecha_Pedido;
-    }
-
-    public void setFecha_Pedido(String fecha_Pedido) {
-        this.fecha_Pedido = fecha_Pedido;
-    }
-
-    public String getFecha_Entrega() {
-        return fecha_Entrega;
-    }
-
-    public void setFecha_Entrega(String fecha_Entrega) {
-        this.fecha_Entrega = fecha_Entrega;
     }
 
     public String getForma_Envio() {
@@ -69,6 +53,20 @@ public class PedidoVO {
         this.forma_Envio = forma_Envio;
     }
 
-    
-    
+    public Date getFecha_Pedido() {
+        return fecha_Pedido;
+    }
+
+    public void setFecha_Pedido(Date fecha_Pedido) {
+        this.fecha_Pedido = fecha_Pedido;
+    }
+
+    public Date getFecha_Entrega() {
+        return fecha_Entrega;
+    }
+
+    public void setFecha_Entrega(Date fecha_Entrega) {
+        this.fecha_Entrega = fecha_Entrega;
+    }
+
 }
