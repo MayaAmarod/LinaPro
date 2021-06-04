385adab19c73cd761fc2149419ce1f9bb0029d02
Jose Dominguez <jdominguez758@misena.edu.co>
2021-06-04 10:12:54 -0500

    Cambio de variables para VO Personas

--- src/java/com/lina/vo/PersonasVO.java
+++ src/java/com/lina/vo/PersonasVO.java
@@ -3,47 +3,41 @@ package com.lina.vo;
 
 
 public class PersonasVO {
-    private String Pusuario, Pcorreo, Ptipodocumento, Pdocumento, Pdireccion, Pcelular, Pcontraseña, PtelefonoFijo, PtipoUsuario;
 
-
-
-    public PersonasVO(String Pusuario, String Pcorreo, String Ptipodocumento, String Pdocumento, String Pdireccion, String Pcelular, String Pcontraseña, String PtelefonoFijo, String PtipoUsuario) {
+    private String Pid_usuario, Pusuario, Pdocumento, Pcorreo, Pdireccion, Pcontraseña,
+            PtelefonoFijo, Pcelular, PtipoUsuario, Ptipodocumento, Pid_estado;
+    
+    public PersonasVO() {
+    }    
+    
+    public PersonasVO(String Pid_usuario, String Pusuario, String Pdocumento, String Pcorreo, String Pdireccion, String Pcontraseña, String PtelefonoFijo, String Pcelular, String PtipoUsuario, String Ptipodocumento, String Pid_estado) {
+        this.Pid_usuario = Pid_usuario;
         this.Pusuario = Pusuario;
-        this.Pcorreo = Pcorreo;
-        this.Ptipodocumento = Ptipodocumento;
         this.Pdocumento = Pdocumento;
+        this.Pcorreo = Pcorreo;
         this.Pdireccion = Pdireccion;
-        this.Pcelular = Pcelular;
         this.Pcontraseña = Pcontraseña;
         this.PtelefonoFijo = PtelefonoFijo;
+        this.Pcelular = Pcelular;
         this.PtipoUsuario = PtipoUsuario;
-    }
-
-    public PersonasVO() {
+        this.Ptipodocumento = Ptipodocumento;
+        this.Pid_estado = Pid_estado;
     }
     
-        public String getPusuario() {
+    public String getPid_usuario() {
+        return Pid_usuario;
+    }
+
+    public void setPid_usuario(String Pid_usuario) {
+        this.Pid_usuario = Pid_usuario;
+    }
+
+    public String getPusuario() {
         return Pusuario;
     }
 
     public void setPusuario(String Pusuario) {
         this.Pusuario = Pusuario;
-    }
-
-    public String getPcorreo() {
-        return Pcorreo;
-    }
-
-    public void setPcorreo(String Pcorreo) {
-        this.Pcorreo = Pcorreo;
-    }
-
-    public String getPtipodocumento() {
-        return Ptipodocumento;
-    }
-
-    public void setPtipodocumento(String Ptipodocumento) {
-        this.Ptipodocumento = Ptipodocumento;
     }
 
     public String getPdocumento() {
@@ -54,20 +48,20 @@ public class PersonasVO {
         this.Pdocumento = Pdocumento;
     }
 
+    public String getPcorreo() {
+        return Pcorreo;
+    }
+
+    public void setPcorreo(String Pcorreo) {
+        this.Pcorreo = Pcorreo;
+    }
+
     public String getPdireccion() {
         return Pdireccion;
     }
 
     public void setPdireccion(String Pdireccion) {
         this.Pdireccion = Pdireccion;
-    }
-
-    public String getPcelular() {
-        return Pcelular;
-    }
-
-    public void setPcelular(String Pcelular) {
-        this.Pcelular = Pcelular;
     }
 
     public String getPcontraseña() {
@@ -86,12 +80,36 @@ public class PersonasVO {
         this.PtelefonoFijo = PtelefonoFijo;
     }
 
+    public String getPcelular() {
+        return Pcelular;
+    }
+
+    public void setPcelular(String Pcelular) {
+        this.Pcelular = Pcelular;
+    }
+
     public String getPtipoUsuario() {
         return PtipoUsuario;
     }
 
     public void setPtipoUsuario(String PtipoUsuario) {
         this.PtipoUsuario = PtipoUsuario;
+    }
+
+    public String getPtipodocumento() {
+        return Ptipodocumento;
+    }
+
+    public void setPtipodocumento(String Ptipodocumento) {
+        this.Ptipodocumento = Ptipodocumento;
+    }
+
+    public String getPid_estado() {
+        return Pid_estado;
+    }
+
+    public void setPid_estado(String Pid_estado) {
+        this.Pid_estado = Pid_estado;
     }
     
 }
